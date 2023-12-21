1e138abfaa44972d4f8d091da372a14fd35db28a
kat <kat@pop-os.localdomain>
2023-12-21 17:23:20 +0000

    Add state for conditionally render edit / save profile button.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ function App() {
       favSpecies: 'Africans and Emperors',
     }
   );
+  const [editOrSave, setEditOrSave] = useState('edit');
+
+  const handleEditOrSaveChange = () => {
+    if (editOrSave === 'edit') {
+      setEditOrSave('save');
+    } else {
+      setEditOrSave('edit');
+    }
+  };
 
   return (
     <div className="container">
@@ -15,7 +24,9 @@ function App() {
       <p>Name: {profile.name}</p>
       <p>Email: {profile.email}</p>
       <p>Favourite penguin species: {profile.favSpecies}</p>
-      <button type="button">Edit profile</button>
+      <button type="button" onClick={() => handleEditOrSaveChange()}>
+        {editOrSave === 'edit' ? 'Edit profile' : 'Save profile'}
+      </button>
     </div>
   );
 }
